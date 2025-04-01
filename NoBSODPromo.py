5392dac1616c428ed6ae998186b7d7ff9ea85410
not.yoshi <76915597+notyoshi-py@users.noreply.github.com>
2025-04-01 20:46:46 +0300

    New Fixed Source File!

--- NoBSODPromo.py
+++ NoBSODPromo.py
@@ -1,5 +1,5 @@
 import winreg as reg
-import pyuac, Win32Security
+import pyuac
 import colorama
 from colorama import Back, Fore, Style
 import os
@@ -12,10 +12,8 @@ splash = r'''
 | . ` |/ _ \  |  __/ '__/ _ \| '_ ` _ \ / _ \    | || '_ \  | ___ \ `--. \| | | | | | |
 | |\  | (_) | | |  | | | (_) | | | | | | (_) |  _| || | | | | |_/ //\__/ /\ \_/ / |/ / 
 \_| \_/\___/  \_|  |_|  \___/|_| |_| |_|\___/   \___/_| |_| \____/ \____/  \___/|___/  
-                                                                                       
 Created by not.yoshi
 My RU TG channels - https://t.me/drop_yoshi
-
 
 
 '''
@@ -23,46 +21,61 @@ My RU TG channels - https://t.me/drop_yoshi
 colorama.init(autoreset=True)
 
 def DisablePromo():
-    with r"SYSTEM\CurrentControlSet\Control\CrashControl" as reg_path:
-        key = reg.OpenKeyEx("HKEY_LOCAL_MACHINE", reg_path, 0, reg.KEY_WRITE)
+    try:
+        reg_path = r"SYSTEM\CurrentControlSet\Control\CrashControl"
+        key = reg.OpenKey(reg.HKEY_LOCAL_MACHINE, reg_path, 0, reg.KEY_SET_VALUE)
 
         reg.SetValueEx(key, "DisplayParameters", 0, reg.REG_DWORD, 4)
         print(rf"Added DisplayParameters value to HKLM\{reg_path}")
         reg.SetValueEx(key, "DisablePromo", 0, reg.REG_DWORD, 1)
         print(rf"Added DisablePromo value to HKLM\{reg_path}")
-    restartVar = str.lower(input("You need to restart to apply changes, do it now?(Y/N): "))
-    if restartVar == "y":
-        os.system("shutdown /r /t 0")
-    elif restartVar == "n":
-        print("\nYou need to restart manually later. Now you can safely close this app.")
-    else:
-        print("Got other input, taking it as NO")
-        print("\nYou need to restart manually later. Now you can safely close this app.")
+
+        reg.CloseKey(key)
+
+        restart_prompt()
+
+    except Exception as e:
+        print(f"Error: {e}")
 
 def EnablePromo():
-    with r"SYSTEM\CurrentControlSet\Control\CrashControl" as reg_path:
-        key = reg.OpenKeyEx("HKEY_LOCAL_MACHINE", reg_path, 0, reg.KEY_WRITE)
+    try:
+        reg_path = r"SYSTEM\CurrentControlSet\Control\CrashControl"
+        key = reg.OpenKey(reg.HKEY_LOCAL_MACHINE, reg_path, 0, reg.KEY_SET_VALUE)
 
-        reg.DeleteValue(key, "DisplayParameters")
-        print(rf"Removed DisplayParameters value from HKLM\{reg_path}")
-        reg.DeleteValue(key, "DisablePromo")
-        print(rf"Removed DisablePromo value from HKLM\{reg_path}")
-    restartVar = str.lower(input("You need to restart to apply changes, do it now?(Y/N): "))
-    if restartVar == "y":
+        try:
+            reg.DeleteValue(key, "DisplayParameters")
+            print(rf"Removed DisplayParameters value from HKLM\{reg_path}")
+        except FileNotFoundError:
+            print("DisplayParameters value not found, skipping...")
+
+        try:
+            reg.DeleteValue(key, "DisablePromo")
+            print(rf"Removed DisablePromo value from HKLM\{reg_path}")
+        except FileNotFoundError:
+            print("DisablePromo value not found, skipping...")
+
+        reg.CloseKey(key)
+
+        restart_prompt()
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+def restart_prompt():
+    restart_var = input("You need to restart to apply changes, do it now? (Y/N): ").strip().lower()
+    if restart_var == "y":
         os.system("shutdown /r /t 0")
-    elif restartVar == "n":
-        print("\nYou need to restart manually later. Now you can safely close this app.")
     else:
-        print("Got other input, taking it as NO")
-        print("\nYou need to restart manually later. Now you can safely close this app.")
+        print("Restart manually later. You can safely close this app.")
 
+# Ensure script is running as Administrator
 if not pyuac.isUserAdmin():
     pyuac.runAsAdmin()
+    exit()
 
 print(splash)
-
-print(Back.RED + "THIS IS NOT SUPPOSED TO HARM DEVICE, HOWEVER, YOU ARE RESPONSIBLE FOR ALL ACTIONS AND ACT ON YOUR OWN RISK")
-continueVar = input(Back.WHITE + Fore.BLACK + "Press ENTER to continue" + Style.RESET_ALL)
+print(Back.RED + "WARNING: This modifies system registry. You proceed at your own risk.")
+input(Back.WHITE + Fore.BLACK + "Press ENTER to continue" + Style.RESET_ALL)
 
 os.system("cls")
 
@@ -71,17 +84,17 @@ print("Disable/Enable promo ads in new Windows 11 Crash Screen!\n")
 
 print("1. Disable promo in crash screen")
 print("2. Enable promo in crash screen\n")
+
 while True:
     try:
-        actionChoose = int(input("Choose next action to do:"))
-        if actionChoose in [1, 2]:
+        action_choose = int(input("Choose an action: "))
+        if action_choose in [1, 2]:
             break
-        else:
-            print("\nGot wrong number, please type correct one instead.")
+        print("Invalid number, try again.")
     except ValueError:
-        print("\nGot wrong input, please type correct one instead.")
+        print("Invalid input, try again.")
 
-if actionChoose == 1:
+if action_choose == 1:
     DisablePromo()
-elif actionChoose == 2:
+else:
     EnablePromo()
